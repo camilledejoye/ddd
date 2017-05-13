f7b28bb3682e47bb15c4bfb1654c8f71aa51474a
elythyr <camilledejoye@gmail.com>
2017-05-14 00:26:59 +0200

    Add a DomainEvent class.

---
+++ src/ddd/Event/DomainEvent.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace ddd\Event;
+
+/**
+ * Basic implementation for a domain's event.
+ *
+ * @author cdejoye
+ */
+class DomainEvent
+{
+
+    /**
+     * @var \DateTimeImmutable The date when an event occured.
+     */
+    private $occuredOn;
+    
+    /**
+     * Gets the date when the event occured.
+     * 
+     * @return \DateTimeImmutable The date.
+     */
+    public function occuredOn()
+    {
+        return $this->occuredOn;
+    }
+
+}
